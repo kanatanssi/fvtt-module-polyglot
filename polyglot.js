e1a53c9bf41a5fa413bd242c7772c218dd23025a
ville <ville@foundry.foundry.kotiverro>
2020-11-16 22:35:31 +0200

    RM specific changes

--- polyglot.js
+++ polyglot.js
@@ -273,7 +273,7 @@ class PolyGlot {
                 break;
             case "pf1":
             case "pf2e":
-                this.loadLanguages("golarion");
+                this.loadLanguages("rm");
                 break;
             case "ose":
                 this.loadLanguages("ose");
